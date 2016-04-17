f1ad35994f34d06d859fdd060d512cc20b9ac03e
Eric Wang <gnawrice@gmail.com>
2016-04-16 20:28:53 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# zsh-lightrise
+
+Super simple zsh prompt based on Oh My Zsh's
+[sunrise theme](https://github.com/robbyrussell/oh-my-zsh/wiki/Themes#sunrise).
+
+![Screenshot](screenshot.png)
+
+## Features
+- Git information via zsh's builtin
+[vcs_info](http://zsh.sourceforge.net/Doc/Release/User-Contributions.html#Version-Control-Information)
+- Only the current and parent directories are shown so the prompt doesn't get
+  overly long
+- No dependencies or frameworks required
+
+## Installation
+### [zplug](https://github.com/b4b4r07/zplug) (recommended)
+`zplug "ericdwang/zsh-lightrise"`
+
+### [zgen](https://github.com/tarjoilija/zgen)
+`zgen load ericdwang/zsh-lightrise`
+
+### [antigen](https://github.com/zsh-users/antigen)
+`antigen bundle ericdwang/zsh-lightrise`
+
+### Manual
+`source zsh-lightrise.zsh`
+
+## License
+[BSD 3-Clause](https://opensource.org/licenses/BSD-3-Clause)
---
+++ zsh-lightrise.zsh
@@ -0,0 +1,28 @@
+# Enable Git support
+autoload -Uz vcs_info
+zstyle ":vcs_info:*" enable git
+
+# Check for staged and unstaged changes
+zstyle ":vcs_info:git:*" check-for-changes true
+zstyle ":vcs_info:git:*" stagedstr "%F{green}+%f"
+zstyle ":vcs_info:git:*" unstagedstr "%F{red}*%f"
+
+# Check whether the current branch has diverged from its remote branch
+zstyle ":vcs_info:git*+set-message:*" hooks git-status
+function +vi-git-status() {
+    origin="$(git rev-parse "@{u}" 2>/dev/null)"
+    if [[ "$origin" && "$origin" != "$(git rev-parse @)" ]]; then
+        hook_com[misc]="%F{blue}#%f"
+    fi
+}
+
+# Git section format
+git_format=" %F{yellow}(%b%f%u%c%m%F{yellow})%f"
+zstyle ":vcs_info:git:*" formats "$git_format"
+zstyle ":vcs_info:git:*" actionformats "$git_format [%a]"
+
+# Full prompt format
+precmd() {
+    vcs_info
+    PROMPT="%F{white}%B--- %2~%f%b${vcs_info_msg_0_} %F{white}%B>%f%b "
+}
